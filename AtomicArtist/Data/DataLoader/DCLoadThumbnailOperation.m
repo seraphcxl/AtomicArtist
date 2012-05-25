676b7404ee38f0ea21f405cc0f090d59324350fa
Derek Chen <seraph.cxl@gmail.com>
2012-05-25 11:12:01 +0800

    Add operation for item view.

---
+++ AtomicArtist/Data/DataLoader/DCLoadThumbnailOperation.m
@@ -0,0 +1,13 @@
+//
+//  DCLoadThumbnailOperation.m
+//  AtomicArtist
+//
+//  Created by Chen XiaoLiang on 12-5-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "DCLoadThumbnailOperation.h"
+
+@implementation DCLoadThumbnailOperation
+
+@end
