b340047e6d5a9e507b744c88e14234e159d34d52
Chen XiaoLiang <seraphcxl@me.com>
2012-12-09 15:03:37 +0800

    config workspace. Add App for Automatic and Tourbillon.

---
+++ AutomaticApp/AutomaticApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AutomaticApp
+//
+//  Created by Chen XiaoLiang on 12/9/12.
+//  Copyright (c) 2012 Chen XiaoLiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DCAppDelegate class]));
+    }
+}
---
+++ TourbillonApp/TourbillonApp/DCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DCAppDelegate.h
+//  TourbillonApp
+//
+//  Created by Chen XiaoLiang on 12/9/12.
+//  Copyright (c) 2012 Chen XiaoLiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
