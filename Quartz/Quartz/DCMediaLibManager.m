77a7624a3779d0cbf308dc173db75576a9249762
Chen XiaoLiang <seraphcxl@me.com>
2012-12-25 21:40:41 +0800

    Update Quartz.

--- Quartz/Quartz/DCMediaLibManager.m
+++ Quartz/Quartz/DCMediaLibManager.m
@@ -52,24 +52,26 @@
         if (self) {
             _threadID = [threadID copy];
             
-            // Read in AtomicArtistModel.xcdatamodeld
-            _model = [NSManagedObjectModel mergedModelFromBundles:nil];
-            // debug_NSLog(@"model = %@", model);
-            
-            NSPersistentStoreCoordinator *psc = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:_model];
-            
-            // Where does the SQLite file go?
-            NSError *error = nil;
-            if (![psc addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:[NSURL fileURLWithPath:[DCMediaLibManager archivePath]] options:nil error:&error]) {
-                [NSException raise:@"MediaLibManger error" format:@"Reason: %@", [error localizedDescription]];
+            @synchronized(_context) {
+                // Read in AtomicArtistModel.xcdatamodeld
+                _model = [NSManagedObjectModel mergedModelFromBundles:nil];
+                // debug_NSLog(@"model = %@", model);
+                
+                NSPersistentStoreCoordinator *psc = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:_model];
+                
+                // Where does the SQLite file go?
+                NSError *error = nil;
+                if (![psc addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:[NSURL fileURLWithPath:[DCMediaLibManager archivePath]] options:nil error:&error]) {
+                    [NSException raise:@"DCMediaLibManager error" format:@"Reason: %@", [error localizedDescription]];
+                }
+                
+                // Create the managed object context
+                _context = [[NSManagedObjectContext alloc] init];
+                [_context setPersistentStoreCoordinator:psc];
+                
+                // The managed object context can manage undo, but we don't need it
+                [_context setUndoManager:nil];
             }
-            
-            // Create the managed object context
-            _context = [[NSManagedObjectContext alloc] init];
-            [_context setPersistentStoreCoordinator:psc];
-            
-            // The managed object context can manage undo, but we don't need it
-            [_context setUndoManager:nil];
         }
         result = self;
     } while (NO);
