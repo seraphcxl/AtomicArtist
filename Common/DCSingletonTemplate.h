d2880bd3bd1fdfcbe2c230228853a4046bd65a4c
Derek Chen <seraph.cxl@gmail.com>
2012-12-28 15:45:29 +0800

    Update for use new singleton template.

--- Common/DCSingletonTemplate.h
+++ Common/DCSingletonTemplate.h
@@ -1,6 +1,6 @@
 //
 //  DCSingletonTemplate.h
-//  
+//
 //
 //  Created by Chen XiaoLiang on 12-12-28.
 //
@@ -9,20 +9,15 @@
 #ifndef _DCSingletonTemplate_h
 #define _DCSingletonTemplate_h
 
-#if __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_5_0 && !defined (DC_DONT_USE_ARC_WEAK_FEATURE)
-
-#define DC_SINGLETON_SAFERELEASE(className)
-
-#else
-
-#define DC_SINGLETON_SAFERELEASE(className) {[shared##className release]; shared##className = nil;}
-
-#endif
-
 #define DEFINE_SINGLETON_FOR_HEADER(className) \
 \
 + (className *)shared##className; \
 + (void)staticRelease; \
+
+#endif
+
+
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_5_0 && !defined (DC_DONT_USE_ARC_WEAK_FEATURE)
 
 #define DEFINE_SINGLETON_FOR_CLASS(className) \
 \
@@ -36,8 +31,50 @@ shared##className = [[self alloc] init]; \
 return shared##className; \
 } \
 \
++ (void)staticRelease {}
+
+#else
+
+#define DEFINE_SINGLETON_FOR_CLASS(className) \
+\
+static className *shared##className = nil; \
+\
++ (className *)shared##className { \
+@synchronized(self) { \
+if (shared##className == nil) { \
+[[self alloc] init]; \
+} \
+} \
+return shared##className; \
+} \
+\
 + (void)staticRelease { \
-DC_SINGLETON_SAFERELEASE(className); \
-}
+@synchronized(self) { \
+if (shared##className) { \
+[shared##className dealloc]; \
+shared##className = nil; \
+} \
+} \
+} \
+\
++ (id)allocWithZone:(NSZone *)zone { \
+@synchronized(self) { \
+if (shared##className == nil) { \
+shared##className = [super allocWithZone:zone]; \
+return shared##className; \
+} \
+} \
+return nil; \
+} \
+\
+- (id)copyWithZone:(NSZone *)zone { return self; } \
+\
+- (id)retain { return self; } \
+\
+- (unsigned)retainCount { return UINT_MAX; } \
+\
+- (oneway void)release {} \
+\
+- (id)autorelease { return self; }
 
 #endif
