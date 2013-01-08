3fe5a399a2471dedc71e58ab6668de20055496fd
Derek Chen <seraph.cxl@gmail.com>
2013-01-08 14:37:42 +0800

    Add Ares.

---
+++ Ares/Ares/DCPullOutView.h
@@ -0,0 +1,65 @@
+//
+//  DCPullOutView.h
+//  Ares
+//
+//  Created by Chen XiaoLiang on 13-1-8.
+//  Copyright (c) 2013年 Chen XiaoLiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import "SafeARC.h"
+#import "DCCommonConstants.h"
+
+#define DCPULLOUTVIEW_ActionSwitchHeight (8.0)
+#define DCPULLOUTVIEW_DefultEdgeInset UIEdgeInsetsMake(8.0, 16.0, 8.0, 16.0)
+#define DCPULLOUTVIEW_IconContentSize (64.0)
+#define DCPULLOUTVIEW_Height (DCPULLOUTVIEW_IconContentSize + DCPULLOUTVIEW_DefultEdgeInset.top + DCPULLOUTVIEW_DefultEdgeInset.bottom + DCPULLOUTVIEW_ActionSwitchHeight)
+#define DCPULLOUTVIEW_InnerSpacing (4.0)
+#define DCPULLOUTVIEW_TitleLabelHeight (32.0)
+#define DCPULLOUTVIEW_DetailTextLabelHeight (24.0)
+
+#define DCPULLOUTVIEW_TextColor [UIColor colorWithRed:DC_RGB(87.0) green:DC_RGB(108.0) blue:DC_RGB(137.0) alpha:1.0]
+#define DCPULLOUTVIEW_FlipAnimationDuration 0.2f
+typedef enum{
+    DCPullOutViewState_Common = 0,
+	DCPullOutViewState_Pulling,
+	DCPullOutViewState_Loading,
+} DCPullOutViewState;
+
+@class DCPullOutView;
+
+@protocol DCPullOutViewDelegate <NSObject>
+
+@required
+- (NSString *)dcPullOutViewDataSourceTitle;
+- (NSString *)dcPullOutViewDataSourceDetailText;
+
+- (void)dcPullOutViewDidTriggerRefresh:(DCPullOutView *)view;
+- (BOOL)dcPullOutViewDataSourceIsLoading:(DCPullOutView *)view;
+
+@optional
+- (NSDate *)dcPullOutViewDataSourceLastUpdated:(DCPullOutView *)view;
+
+@end
+
+@interface DCPullOutView : UIView {
+}
+
+@property (nonatomic, assign) DCPullOutViewState state;
+
+@property (nonatomic, SAFE_ARC_PROP_WEAK) id<DCPullOutViewDelegate> delegate;
+
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *detailText;
+
+@property (nonatomic, readonly) UILabel *titleLabel;
+@property (nonatomic, readonly) UILabel *detailTextLabel;
+@property (nonatomic, readonly) UIImageView *arrowView;
+@property (nonatomic, readonly) UIActivityIndicatorView *activityIndicatorView;
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView;
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView;
+- (void)scrollViewDataSourceDidFinishedLoading:(UIScrollView *)scrollView;
+
+@end
