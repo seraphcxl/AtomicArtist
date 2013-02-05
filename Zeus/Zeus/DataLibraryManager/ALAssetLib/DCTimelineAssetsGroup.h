dd04541d9c9c2be76bc5c22018104b2d2b7121d2
Derek Chen <seraph.cxl@gmail.com>
2013-02-05 14:41:33 +0800

    Update Zeus.

---
+++ Zeus/Zeus/DataLibraryManager/ALAssetLib/DCTimelineAssetsGroup.h
@@ -0,0 +1,23 @@
+//
+//  DCTimelineAssetsGroup.h
+//  Zeus
+//
+//  Created by Chen XiaoLiang on 13-2-5.
+//  Copyright (c) 2013年 Chen XiaoLiang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DCUniformDataProtocol.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+#import "DCCommonConstants.h"
+#import "SafeARC.h"
+#import "DCALAssetsGroupBase.h"
+
+@class DCALAssetItem;
+
+@interface DCTimelineAssetsGroup : DCALAssetsGroupBase <DCTimelineDataGroup> {
+}
+
+- (void)insertDataItem:(DCALAssetItem *)assetItem;
+
+@end
---
+++ Zeus/Zeus/DataLibraryManager/ALAssetLib/DCTimelineAssetsGroup.m
@@ -0,0 +1,59 @@
+//
+//  DCTimelineAssetsGroup.m
+//  Zeus
+//
+//  Created by Chen XiaoLiang on 13-2-5.
+//  Copyright (c) 2013年 Chen XiaoLiang. All rights reserved.
+//
+
+#import "DCTimelineAssetsGroup.h"
+
+@interface DCTimelineAssetsGroup () {
+}
+
+@end
+
+@implementation DCTimelineAssetsGroup
+
+@synthesize earliestTime = _earliestTime;
+@synthesize latestTime = _latestTime;
+
+- (void)insertDataItem:(DCALAssetItem *)assetItem {
+    do {
+        ;
+    } while (NO);
+}
+
+#pragma mark - DCTimelineAssetsGroup - DCTimelineDataGroup
+- (void)refining:(NSArray *)refinedGroups withTimeInterval:(CFTimeInterval)timeInterval {
+    do {
+        ;
+    } while (NO);
+}
+
+#pragma mark - DCTimelineAssetsGroup - DCDataGroupBase
+- (NSString *)uniqueID {
+    NSString *result = nil;
+    do {
+        @synchronized(self) {
+            ;
+        }
+    } while (NO);
+    return result;
+}
+
+- (NSUInteger)itemsCountWithParam:(id)param {
+    NSUInteger result = 0;
+    do {
+        @synchronized(self) {
+            if (_allAssetItems) {
+                result = [_allAssetItems count];
+            }
+        }
+    } while (NO);
+    return result;
+}
+
+#pragma mark - DCTimelineAssetsGroup - Private
+
+@end
